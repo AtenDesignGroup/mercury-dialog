f26f0362f189ac6090076e5415f78839c0a8b715
John Ferris <john@atendesigngroup.com>
2023-05-05 10:05:39 +0900

    Increase the default viewport offset value.

--- src/mercury-dialog.ts
+++ src/mercury-dialog.ts
@@ -71,8 +71,8 @@ export class MercuryDialog extends LitElement {
       width: var(--me-dialog-width, var(--me-dialog-width-default, fit-content));
       height: var(--me-dialog-height, var(--me-dialog-height-default, fit-content));
       min-width: var(--me-dialog-min-width, min-content);
-      max-width: calc(100vw - var(--me-dialog-viewport-offset, 40px));
-      max-height: calc(100vh - var(--me-dialog-viewport-offset, 40px));
+      max-width: calc(100vw - var(--me-dialog-viewport-offset, 80px));
+      max-height: calc(100vh - var(--me-dialog-viewport-offset, 80px));
       z-index: 1000;
     }
 
